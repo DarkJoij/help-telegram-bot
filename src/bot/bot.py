48c80616b130fdbf3c11929239e949e6415555e4
DarkJoij <aleksey.c5@yandex.ru>
2025-02-20 01:13:11 +0300

    Recompiled responses and added "back" buttons. modified:   src/bot/bot.py modified:   src/bot/utils/utils.py

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,10 +1,11 @@
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import CallbackQuery, InlineKeyboardMarkup, Message
 
-from .utils import *
+from .utils import (Menu, MenuType, Phrases,
+                    empty_button_callback, main_menu, nacs_menu, semester_menu, telecommunications_menu)
 
 
 async def edit(message: Message, text: str) -> None:
@@ -12,82 +13,102 @@ async def edit(message: Message, text: str) -> None:
 
 
 class TelegramBot:
-    def __init__(self, config, logger):
+    def __set_callback_text(self, callback_data: str) -> bool:
+        self.__callback_text = callback_data
+        return True
+
+    def __get_callback_response_text(self) -> str:
+        match self.__callback_text:
+            case "nacs":
+                self.__callback_markup_menu_type = MenuType.NACS
+                return Phrases.nacs_text
+            case "telecommunications":
+                self.__callback_markup_menu_type = MenuType.TELECOMMUNICATIONS
+                return Phrases.telecommunications_text
+            case "go_back":
+                match self.__callback_markup_menu_type:
+                    case MenuType.NACS:
+                        self.__callback_markup_menu_type = MenuType.MAIN
+                        return Phrases.main_text
+                    case MenuType.TELECOMMUNICATIONS:
+                        self.__callback_markup_menu_type = MenuType.NACS
+                        return Phrases.nacs_text
+                    case MenuType.SEMESTER:
+                        self.__callback_markup_menu_type = MenuType.TELECOMMUNICATIONS
+                        return Phrases.telecommunications_text
+                    case _:
+                        return Phrases.main_text
+            case _:
+                if self.__callback_text.startswith("semester"):
+                    self.__callback_markup_menu_type = MenuType.SEMESTER
+
+                    match self.__callback_text:
+                        case "semester_1":
+                            return Phrases.semester_1
+                        case "semester_2":
+                            return Phrases.semester_2
+                        case "semester_3":
+                            return Phrases.semester_3
+                        case "semester_4":
+                            return Phrases.semester_4
+                        case "semester_5":
+                            return Phrases.semester_5
+                        case "semester_6":
+                            return Phrases.semester_6
+                        case "semester_7":
+                            return Phrases.semester_7
+                        case "semester_8":
+                            return Phrases.semester_8
+                        
+                self.__callback_markup_menu_type = MenuType.EMPTY
+                return "__get_callback_response_text_erroneous_example"  # FIXME: Temporary value. 
+
+    def __get_callback_markup_menu(self) -> Menu:
+        match self.__callback_markup_menu_type:
+            case MenuType.EMPTY:
+                return []
+            case MenuType.MAIN:
+                return main_menu
+            case MenuType.NACS:
+                return nacs_menu
+            case MenuType.TELECOMMUNICATIONS:
+                return telecommunications_menu
+            case MenuType.SEMESTER:
+                return semester_menu
+
+    def __init__(self, config, logger) -> None:
         self.config = config
         self.logger = logger
         self.dispatcher = Dispatcher()
+        
         self.__bot = Bot(
             token=self.config['tech']['bot_token'],
             default=DefaultBotProperties(parse_mode=ParseMode.HTML)
         )
 
+        self.__callback_text = str()
+        self.__callback_markup_menu_type: MenuType = MenuType.MAIN
+        
+        
     def handlers(self) -> None:
         @self.dispatcher.message(CommandStart())
         async def start_command_handler(message: Message) -> None:
+            self.__callback_markup_menu_type = MenuType.MAIN
+
             await message.answer(
-                "Выбери факультет:", 
-                reply_markup=InlineKeyboardMarkup(inline_keyboard=main_menu)
+                Phrases.main_text, 
+                reply_markup=InlineKeyboardMarkup(inline_keyboard=self.__get_callback_markup_menu())
             )
-        
-        @self.dispatcher.callback_query(lambda c: c.data == "nacs")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text(
-                "Выбери направление факультета СИСС:", 
-                reply_markup=InlineKeyboardMarkup(inline_keyboard=nacs_menu)
-            )
-            await callback.answer()
 
-        @self.dispatcher.callback_query(lambda c: c.data == "telecommunications")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text(
-                "Выбери семестр:", 
-                reply_markup=InlineKeyboardMarkup(inline_keyboard=telecommunications_menu)
-            )
-            await callback.answer()
+        @self.dispatcher.callback_query(lambda c: self.__set_callback_text(c.data))
+        async def callback_handler(callback: CallbackQuery) -> None:
+            text = self.__get_callback_response_text()
+            markup = InlineKeyboardMarkup(inline_keyboard=self.__get_callback_markup_menu())
 
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_1")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 1-м семестре")
-            await callback.answer()
+            if text == empty_button_callback:
+                markup = InlineKeyboardMarkup()
 
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_2")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 2-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_3")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 3-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_4")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 4-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_5")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 5-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_6")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 6-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_7")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 7-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data == "semester_8")
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await callback.message.edit_text("Информация о 8-м семестре")
-            await callback.answer()
-
-        @self.dispatcher.callback_query(lambda c: c.data.startswith("_"))
-        async def start_command_handler(callback: CallbackQuery) -> None:
-            await edit(callback.message, "In devepoment.")
+            await callback.message.edit_text(text, reply_markup=markup)
             await callback.answer()
 
     async def run(self):
--- src/bot/utils/utils.py
+++ src/bot/utils/utils.py
@@ -1,21 +1,48 @@
-from aiogram.types import (InlineKeyboardButton,
-                           InlineKeyboardMarkup,
-                           KeyboardButton,
-                           ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from aiogram.types import InlineKeyboardButton
 
-__empty_button_callback = "_"  # FIXME: Just for test. Later will be deleted.
+from dataclasses import dataclass
+from enum import Enum
+from typing import TypeAlias
 
-main_menu = [
+empty_button_callback = "_empty_button_callback"  # FIXME: Just for test. Later will be deleted.
+go_back_button = InlineKeyboardButton(text="Назад", callback_data="go_back")
+
+Menu: TypeAlias = list[list[InlineKeyboardButton]]
+
+
+class MenuType(Enum):
+    EMPTY = 0
+    MAIN = 1
+    NACS = 2
+    TELECOMMUNICATIONS = 3
+    SEMESTER = 4
+
+
+@dataclass
+class Phrases:
+    main_text: str = "Выбери факультет:"
+    nacs_text: str = "Выбери направление факультета СИСС:"
+    telecommunications_text: str = "Выбери семестр:"
+    semester_1: str = "Информация о 1-м семестре."
+    semester_2: str = "Информация о 2-м семестре."
+    semester_3: str = "Информация о 3-м семестре."
+    semester_4: str = "Информация о 4-м семестре."
+    semester_5: str = "Информация о 5-м семестре."
+    semester_6: str = "Информация о 6-м семестре."
+    semester_7: str = "Информация о 7-м семестре."
+    semester_8: str = "Информация о 8-м семестре."
+
+
+main_menu: Menu = [
     [InlineKeyboardButton(text="СИСС", callback_data="nacs")],
-    [InlineKeyboardButton(text="...", callback_data=__empty_button_callback)],
+    [InlineKeyboardButton(text="...", callback_data=empty_button_callback)]
 ]
-
-nacs_menu = [
-    [InlineKeyboardButton(text="Беспроводная связь", callback_data=__empty_button_callback)],
-    [InlineKeyboardButton(text="Телекоммуникации", callback_data="telecommunications")]
+nacs_menu: Menu = [
+    [InlineKeyboardButton(text="Беспроводная связь", callback_data=empty_button_callback)],
+    [InlineKeyboardButton(text="Телекоммуникации", callback_data="telecommunications")],
+    [go_back_button]
 ]
-
-telecommunications_menu = [
+telecommunications_menu: Menu = [
     [InlineKeyboardButton(text="1 семестр", callback_data="semester_1")],
     [InlineKeyboardButton(text="2 семестр", callback_data="semester_2")],
     [InlineKeyboardButton(text="3 семестр", callback_data="semester_3")],
@@ -23,9 +50,10 @@ telecommunications_menu = [
     [InlineKeyboardButton(text="5 семестр", callback_data="semester_5")],
     [InlineKeyboardButton(text="6 семестр", callback_data="semester_6")],
     [InlineKeyboardButton(text="7 семестр", callback_data="semester_7")],
-    [InlineKeyboardButton(text="8 семестр", callback_data="semester_8")]
+    [InlineKeyboardButton(text="8 семестр", callback_data="semester_8")],
+    [go_back_button]
 ]
 
-
-class ButtonCallbackAnswer:
-    pass
+semester_menu = [
+    [go_back_button]
+]
